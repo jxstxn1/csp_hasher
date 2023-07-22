edf55d803a4ce52ce624e692bb736ee816ad3573
Justin Baumann <justin.baumann@t-online.de>
2023-07-22 20:59:01 +0200

    export Hash and used Hashes from crypto package

--- lib/csp_hasher.dart
+++ lib/csp_hasher.dart
@@ -3,5 +3,7 @@
 /// More dartdocs go here.
 library csp_hasher;
 
+export 'package:crypto/crypto.dart' show Hash, sha256, sha384, sha512;
+
 export 'src/csp_hash.dart' show CspHash, HashMode;
 export 'src/csp_hasher.dart' show hashScripts;
